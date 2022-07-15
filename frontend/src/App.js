08c34f9cf3fb03b76c664c140372a8510ecae861
Steve <stevealv@hotmail.com>
2022-07-15 12:10:54 -0600

    [Fix] Link tag

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -26,17 +26,17 @@ function App() {
     <div className="App">
       <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container-fluid">
-          <a class="navbar-brand" href="/home">Gold Tracker</a>
+          <Link class="navbar-brand" to="/home">Gold Tracker</Link>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
           <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav">
               <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="/home">Home</a>
+                <Link class="nav-link active" aria-current="page" to="/home">Home</Link>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="/">Logout</a>
+                <Link class="nav-link" to="/">Logout</Link>
               </li>
             </ul>
           </div>
