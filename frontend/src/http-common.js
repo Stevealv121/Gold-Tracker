a61d0c13503b38908e1c537994129dc720cc6875
Steve <stevealv@hotmail.com>
2022-08-31 19:48:07 -0600

    [Update] Migration to google cloud

--- frontend/src/http-common.js
+++ frontend/src/http-common.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: 'https://goldtrackerapi.herokuapp.com/api/v1',
+    baseURL: 'https://goldentrackerapi.ue.r.appspot.com/api/v1',
     //baseURL: 'http://localhost:5000/api/v1',
     headers: {
         "Content-type": "application/json"
